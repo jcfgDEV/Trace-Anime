0dbac5a2c5aa5152656aa36989080fe6722db22d
jcfgDEV <john.f@outlook.com.br>
2022-12-23 20:03:57 +0000

    Change of image ico

--- app/head.js
+++ app/head.js
@@ -1,9 +1,10 @@
+import s from '../public/favicon.ico'
 export default function Head() {
   return (
     <>
       <title>Trace Anime</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-	    <link rel="icon" type="image/png" sizes="32x32" href="../public/gundam.png" />
+	    <link rel="icon" type="image/x-icon" href="../public/favicon.ico"></link>
 	    <meta name="msapplication-TileColor" content="#da532c" />
 	    <meta name="theme-color" content="#ffffff"></meta>
     </>
